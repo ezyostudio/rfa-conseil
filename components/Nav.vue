a74fe3c56f87e1a6f6be150a2577689f594962bb
Zao Soula <contact@zaosoula.fr>
2021-09-07 12:48:03 +0200

    0.0.1

---
+++ components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary d-none d-md-block">
+      <div class="container-fluid px-5">
+        <ul class="ms-auto navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <icon-tel /> 09 51 74 07 79
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">
+              <icon-mail /> regis.frachier@rfa-conseil.fr
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+      <div class="container-fluid px-5">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+            class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">EXPERTISE</a></li>
+            <li class="nav-item"><a class="nav-link" href="#!">PRESTATION</a></li>
+            <li class="nav-item"><a class="nav-link" href="#!">CONTACT</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ pages/bootstrap.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container" style="background: blue; height: 100vh">
+    <div class="row">
+      <div class="col-12 col-md-7 red"></div>
+      <div class="col blue"></div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss">
+  .blue, .red {
+    height: 300px;
+  }
+
+  .blue {
+    background: violet;
+  }
+
+  .red {
+    background: red;
+  }
+</style>
