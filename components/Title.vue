c63082baea1f1cb9adb2afc135ca57cffd1f56d2
Tristan264 <tristan.bschwenk@icloud.com>
2021-09-07 18:39:25 +0200

    Created components

---
+++ components/Title.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p class="text-light">{{text}}</p>
+    </div>
+</template>
+
+
+<style scoped>
+    div {
+        background-color: #00AEEF;
+        width: auto;
+        box-sizing: border-box;
+    }
+
+    p {
+        margin: 0;
+        font-size: 20px;
+    }
+</style>
+
+<script>
+export default {
+    props: ['text']
+}
+</script>
