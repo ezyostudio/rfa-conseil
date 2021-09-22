72075e616d583037c0f80e071d629fcda1877511
Zao Soula <contact@zaosoula.fr>
2021-09-23 01:48:21 +0200

    fix(generate): relative paths

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/rfa-conseil/'
+  },
+
   generate: {
     dir: './dist',
     interval: 2000, // fix nuxt composition api bug
