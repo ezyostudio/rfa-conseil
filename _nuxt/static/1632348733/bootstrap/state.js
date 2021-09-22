d923b0f1cdec9686ef48e02c04faa367588bcbbe
zaosoula <zaosoula@users.noreply.github.com>
2021-09-22 22:12:19 +0000

    deploy: b0a62f607cfcf54a2ad8adfebf79295282897346

---
+++ _nuxt/static/1632348733/bootstrap/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frfa-conseil\u002F_nuxt\u002Fstatic\u002F1632348733",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbootstrap",config:{_app:{basePath:"\u002Frfa-conseil\u002F",assetsPath:"\u002Frfa-conseil\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e7ce1008"}},globalRefs:{}}}(null));
