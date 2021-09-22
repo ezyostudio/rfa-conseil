ad664a16254dfecccdf0f5dec0ab5d50fdfe75d1
zaosoula <zaosoula@users.noreply.github.com>
2021-09-22 20:50:59 +0000

    deploy: b459cbe9ad7f98bb3d4e313ba36cba76d6ab74b3

---
+++ _nuxt/static/1632343851/bootstrap/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frfa-conseil\u002F_nuxt\u002Fstatic\u002F1632343851",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbootstrap",config:{_app:{basePath:"\u002Frfa-conseil\u002F",assetsPath:"\u002Frfa-conseil\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"66f2fc77"}},globalRefs:{}}}(null));
--- components/icon/arrow-left.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <svg width="26" height="24" viewBox="0 0 26 24" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M9.50293 6L15.7349 12L9.50293 18" stroke-width="2" stroke-linecap="round"
-      stroke-linejoin="round" />
-  </svg>
-</template>
