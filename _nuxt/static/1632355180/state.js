8e385e40fb4ce68a514f9e96456f0021c2fcd27a
tristanschwenk <tristanschwenk@users.noreply.github.com>
2021-09-24 10:14:41 +0000

    deploy: 9b24e42d28b6d689850c40fa2f0d40ab11702661

--- _nuxt/static/1632355180/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632355180",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9eb33623"}},globalRefs:{}}}(null,"\u002F"));
