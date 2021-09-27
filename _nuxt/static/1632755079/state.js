cf3d92ec0cda08625ee5259ed4fe6b0e5c8b65d2
zaosoula <zaosoula@users.noreply.github.com>
2021-09-27 15:07:40 +0000

    deploy: 4df0b852c4a7e7d78068a7dba452e35c60be0bd7

--- _nuxt/static/1632755079/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632755079",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4a7298a6"}},globalRefs:{}}}(null,"\u002F"));
