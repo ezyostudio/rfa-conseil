1e920ca7c2ee31b1c845809f7ea4df999b2f76c5
Zao Soula <contact@zaosoula.fr>
2021-10-11 11:44:32 +0200

    Update lazy.js

--- plugins/lazy.js
+++ plugins/lazy.js
@@ -5,18 +5,4 @@ Vue.use(VueLazyload, {
   preLoad: 1.3,
   attempt: 1,
   silent: false,
-  adapter: {
-    loaded ({ bindType, el, naturalHeight, naturalWidth, $parent, src, loading, error, Init }) {
-      console.log('loaded')
-
-    },
-    loading (listender, Init) {
-        console.log('loading')
-    },
-    error (listender, Init) {
-        console.log('error')
-    }
-}
-})
-
-
+}})
