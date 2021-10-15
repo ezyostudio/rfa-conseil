e966b19b55a4d698d22eb180413a423b0af8cf7c
zaosoula <zaosoula@users.noreply.github.com>
2021-10-15 07:53:42 +0000

    deploy: 1ddcee23e52575c740785d5b130f54c84d21f68b

--- _nuxt/static/1634284110/mention-legale/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/mention-legale", {data:[{_img:{}}],fetch:{},mutations:void 0});
--- _nuxt/static/1634284110/mention-legale/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634284110",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmention-legale",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"94ba0c93"}},globalRefs:{}}}(null));
