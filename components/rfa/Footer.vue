0d61283db1cbe3085de43f09d7fb0ca086d96750
Tristan264 <tristan.bschwenk@icloud.com>
2021-10-15 10:50:14 +0200

    Update Footer.vue

--- components/rfa/Footer.vue
+++ components/rfa/Footer.vue
@@ -2,10 +2,10 @@
     <footer class="py-3">
       <div class="container">
         <div class="row">
-          <div class="col">
-            <p class="m-0 text-light">&copy; 2021 RFA CONSEIL</p>
+          <div class="col-12 col-md-6">
+            <p class="m-0 text-light text-center text-md-start">&copy; 2021 RFA CONSEIL</p>
           </div>
-          <div class="col text-end">
+          <div class="col-12 col-md-6 text-center text-md-end">
             <a href="https://go.ezyostudio.com/" target="_blank" class="text-light text-decoration-none">Made with
               <icon-love /> by EzyoStudio</a>
           </div>
