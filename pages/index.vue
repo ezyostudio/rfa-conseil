4fef1019ae19e13da1b5fa05922a5795c26f5d13
Zao Soula <contact@zaosoula.fr>
2021-10-15 09:52:25 +0200

    feat(footer): footer as component

--- pages/index.vue
+++ pages/index.vue
@@ -176,22 +176,7 @@
     <Contact />
   </section>
 
-
-
-    <!-- Footer-->
-    <footer class="py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <p class="m-0 text-light">&copy; 2021 RFA CONSEIL</p>
-          </div>
-          <div class="col text-end">
-            <a href="https://go.ezyostudio.com/" target="_blank" class="text-light text-decoration-none">Made with
-              <icon-love /> by EzyoStudio</a>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <rfa-footer />
   </div>
 </template>
 
